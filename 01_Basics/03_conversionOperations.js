aded570602394179fd5c1b56b1c0f1985ebfc33c
prajwal-godase <prajwalgodase05@gmail.com>
2025-12-21 23:13:26 +0530

    Conversions

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,58 @@
+
+// console.log("Alphabates OR Number and Alphabates together:")
+// let score="33abc"
+// let valueInNumber= Number(score)
+// console.log(valueInNumber) // NaN= Not a number
+// console.log(typeof valueInNumber)
+
+// console.log("For null:")
+// let score1=null
+// let valueInNumber1= Number(score1)
+// console.log(valueInNumber1) // Gives 0
+// console.log(typeof valueInNumber1)
+
+// console.log("For undefined:")
+// let score2=undefined
+// let valueInNumber2= Number(score1)
+// console.log(valueInNumber2) // Gives 0
+// console.log(typeof valueInNumber2)
+
+// // Converting to number:
+// // "33"=> 33
+// // "33abc"=> NaN(Not a Number)-> But NaN's type is number
+// // "true"=1; "false"=>0
+
+
+
+// console.log("For number")
+// let isLoggedIn=1
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+
+// let isLoggedIn1=0
+// let booleanIsLoggedIn1=Boolean(isLoggedIn1)
+// console.log(booleanIsLoggedIn1)
+// console.log(typeof booleanIsLoggedIn1)
+
+// console.log("For string")
+// let isLoggedIn2="Prajwal"
+// let booleanIsLoggedIn2=Boolean(isLoggedIn2)
+// console.log(booleanIsLoggedIn2)
+// console.log(typeof booleanIsLoggedIn2)
+
+// let isLoggedIn3=""
+// let booleanIsLoggedIn3=Boolean(isLoggedIn3)
+// console.log(booleanIsLoggedIn3)
+// console.log(typeof booleanIsLoggedIn3)
+
+// // Converting to boolean:
+// // 1=>true; 0=>false
+// // "aaaa"=>true
+// // ""=>false
+
+let someNumber=33
+let stringSomeNumber=String(someNumber)
+console.log(stringSomeNumber)
+console.log(typeof stringSomeNumber)
+
