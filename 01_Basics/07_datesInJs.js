d522d0fde9271afe8b20e00ef2d5e6db02322084
prajwal-godase <prajwalgodase05@gmail.com>
2025-12-22 23:44:24 +0530

    Dates in Javascript

---
+++ 01_Basics/07_datesInJs.js
@@ -0,0 +1,36 @@
+// let myDate=new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+
+// console.log(typeof myDate);
+
+// let myCreatedDate=new Date(2025,0,20)
+// console.log(myCreatedDate.toDateString());
+
+// let myCreatedDate1=new Date(2025,0,20,23,5,3)
+// console.log(myCreatedDate1.toLocaleString());
+
+// let myCreatedDate2=new Date("2025-01-20")
+// console.log(myCreatedDate2.toLocaleString());
+
+let myCreatedDate3=new Date("01-20-2005")
+// console.log(myCreatedDate3.toLocaleString());
+
+// ++++++++++++++++ TimeStamps +++++++++++++++++
+
+let myTimeStamp=Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate3.getTime());
+console.log(Math.floor(Date.now()/1000));
+
+let newDate=new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
+newDate.toLocaleString("default",{
+    weekday: "long"
+})
