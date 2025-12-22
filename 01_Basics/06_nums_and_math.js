a0a2fc46e353f6e99be6b6b5d8b374ef02b6c317
prajwal-godase <prajwalgodase05@gmail.com>
2025-12-22 23:11:36 +0530

    Maths and Num

---
+++ 01_Basics/06_nums_and_math.js
@@ -0,0 +1,35 @@
+// const score=400
+
+// const balance=new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber =123.89999
+// console.log(otherNumber.toPrecision(3));// To give precise value
+
+// const otherNumber1 =1123.89999
+// console.log(otherNumber1.toPrecision(4));
+
+// const hundreds=1000000
+// console.log(hundreds.toLocaleString());
+
+//+++++++++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4)); 
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.7));
+// console.log(Math.min(4,2,6,4,1));
+// console.log(Math.max(4,2,6,4,1));
+
+console.log(Math.random());// Gives random number between 0 to 1
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min) //For generating in the range 
+
