ed0026b22fdb03a1c5015f7e370d0683d0660590
prajwal-godase <prajwalgodase05@gmail.com>
2025-12-25 10:59:05 +0530

    Functions in the JS

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -20,7 +20,7 @@ function addition(number1,number2){ // Here are parameters
     // return result
     return number1+number2
 }
-const result=addition(2,5) // Here are arguments
+// const result=addition(2,5) // Here are arguments
 // console.log("Result:",result)
 
 function loginUserMessage(username="Sam"){ // Sam as default value
@@ -30,5 +30,34 @@ function loginUserMessage(username="Sam"){ // Sam as default value
     }
     return `${username} just logged in`
 }
-console.log(loginUserMessage("Prajwal")) // If you dont give the argument then it takes the undefined
+// console.log(loginUserMessage("Prajwal")) // If you dont give the argument then it takes the undefined
 
+
+
+function calculateCartPrice(val1,val2, ...num1){// val1 for first, val2 for second and other is in the num1 array
+    // Rest(...) operator (same as the spread) is used to take n number of parameters 
+    // and makes an array of them
+    return num1
+}
+// console.log(calculateCartPrice(200,400,500))
+
+const user={
+    username:"Prajwal",
+    price:199
+}
+
+// function handleObject(anyObject){
+//     console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+// }
+
+// handleObject({
+//     username:"Sam",
+//     price:399
+// })
+
+const myNewArr=[200,400,100,600]
+
+function returnSecondValue(getArr){
+    return getArr[1]
+}
+console.log(returnSecondValue(myNewArr))
