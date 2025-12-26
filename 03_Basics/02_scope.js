c211108f9746c6f097ccdec395f751aa308ac2fa
prajwal-godase <prajwalgodase05@gmail.com>
2025-12-26 13:32:39 +0530

    Scope level and mini hoisting

--- 03_Basics/02_scope.js
+++ 03_Basics/02_scope.js
@@ -4,9 +4,35 @@ if(true){
     let a=10 // Block scope
     const b=20 // Block scope
     // var c=30
-    console.log("Inner a=",a); 
+    // console.log("Inner a=",a); 
 }
 
-console.log("Outer a=",a); 
+// console.log("Outer a=",a); 
 // console.log(b); //Scope for block
 // console.log(c); 
+
+
+// // Nested Scope
+
+// function one(){
+//     const username="Prajwal"
+//     function two(){
+//         const website="Youtube"
+//         console.log(username);
+//     }
+//     // console.log(website); // Out of scope
+//     two()
+// }
+// one()
+
+
+//++++++++++++++++++++++++ intresting ++++++++++++++++++++++++++++++++++++++++++
+console.log(addone(5))
+function addone(num){ // First method
+return num+1
+}
+
+// addtwo(5) // not allowed in this type of function declaration (Mini Hoisting)
+const addtwo=function(num){ // Second method
+return num+2
+}
